fcdf0e0aabbd190e139b2bdd3bd59d58eb3e0b33
James Nguyen <nguyenthecoder@gmail.com>
2022-05-26 15:18:40 -0400

    working with extracting headlines

--- src/service/news_spider/news_spider/bs_playground.py
+++ src/service/news_spider/news_spider/bs_playground.py
@@ -1,3 +1,4 @@
+from asyncio import all_tasks
 from bs4 import BeautifulSoup as bs
 from newspaper import Article
 from datetime import datetime, timedelta
@@ -7,116 +8,38 @@ from newsapi import NewsApiClient
 import pathlib
 import os
 import requests
-import re
 
-SRC_DIR = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
-SECRET_DIR = os.path.join(SRC_DIR, 'secrets', 'newsapi_key.txt')
-
-def news_api_playground():
-    with open(SECRET_DIR, 'r') as file:
-        apikey = file.readline()
-    
-    print(apikey)
-
-    newsapi = NewsApiClient(api_key = apikey)
-
-    headlines = newsapi.get_top_headlines(q='', category='business')
-    print(headlines)
-
-    with open('newsapi_result.json', 'w') as file:
-        json.dump(headlines, file)
-
-    # Process response from newsapi top headlines
-
-    with open('newsapi_result.json', 'r') as file:
-        headlines = json.load(file)
-
-    # print(headlines) 
-    print(headlines['status'])
-    # print(headlines['articles'][0])
-    error_links = list()
-
-    for headline in headlines['articles']:
-        try:
-            article = Article(headline['url'])
-
-            if headline['source']['name'] == 'CNBC':
-                headline['content'] = cnbc_parser(headline['url'])
-
-            else:
-                article.download()
-                article.parse()
-                headline['content'] = article.text
-
-            print(f"got content for {headline['title']}")
-        except Exception as e:
-            print(f" error link: {headline['url']}\n{str(e)}")
-            error_links.append(headline['url'])
-        
-    print('Error links : ' + str(error_links))
-
-    with open('newsapi_result_with_article.json', 'w') as file:
-        json.dump(headlines, file)
-
-def cnbc_parser(link):
-    '''
-    parser specifically for CNBC news
-    updated: 5.25.2022
-
-    @params:
-        string link: link to article
-    
-    @return:
-        string text: parsed text
-    '''
-    response = requests.get(link)
-
-    html = response.text
-
+def extract_links(html):
     soup = bs(html)
 
-    # regex = re.compile('^PageBuilder-article')
-    ArticleBody = soup.find('div', {'class': 'ArticleBody-articleBody'})
+    all_a_tags = soup.find_all('a')
+    hrefs = list()
+    for a in all_a_tags:
+        try:
+            href = a.attrs['href']
+            if href.endswith(f'index.html'):
+                hrefs.append(href)
 
-    divs = ArticleBody.find_all('div', {'class': "group"})
-    text = list()
-
-    for div in divs:
-
-        elements = div.find_all(recursive=True)
-
-        for e in elements:
-            if e.name == 'a' or e.text.startswith('Subscribe to CNBC on YouTube.'):
-                continue
-            
-            if 'Sign up now' in e.text:
-                break
-
-            text.append(e.text)
-
-    return '\n'.join(text)
+        except Exception as e:
+            print(str(e))
+            continue
+    
+    return hrefs
 
 
 if __name__ == '__main__':
-    # googlenews = GoogleNews(lang = 'en', region = 'US')
-    # googlenews.get_news("APPLE")
-    # results = googlenews.results()
-    # for result in results:
-    #     print(result)
-
-    # link = 'https://www.cnbc.com/2022/05/25/ripple-will-explore-ipo-after-sec-lawsuit-ends-ceo-says.html'
-    # # link = 'https://www.cnbc.com/2022/05/25/try-these-gas-saving-tips-before-hitting-the-road-this-memorial-day-weekend.html'
-    # link = 'https://www.cnbc.com/2022/05/25/5-things-to-know-before-the-stock-market-opens-wednesday-may-25.html'
-    # link = 'https://www.cnbc.com/2022/05/25/stocks-making-the-biggest-moves-premarket-dicks-sporting-express-wendys-and-more.html'
-    # link = 'https://www.cnbc.com/2022/05/25/ecb-member-knot-says-a-50-basis-point-hike-in-july-is-not-off-the-table.html'
-
-    # text = cnbc_parser(link)
-    # print(text)
-
-    news_api_playground()
-
-
-
+    with open('cnn_sample.html', 'r') as file:
+        html = file.read()
+    
+    links = extract_links(html)
+    for link in links:
+        link = 'https://www.cnn.com'+link
+        print(link)
+        article = Article(link)
+        article.download()
+        article.parse()
+        print(article.text)
+        break
 
 
 
--- src/service/news_spider/news_spider/spiders/CNNBusinessHeadlines.py
+++ src/service/news_spider/news_spider/spiders/CNNBusinessHeadlines.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup as bs
 import pathlib
 import os
 import nltk
-import uuid
 import logging
 from datetime import datetime, timedelta
 from newspaper import Article
@@ -15,10 +14,8 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import pathlib
-
-#append path to search for database service
-# SERVICE_ROOT = pathlib.Path(__file__).parent.parent.parent.parent
-# sys.path.append(str(SERVICE_ROOT))
+import pandas as pd
+from collections import Counter
 
 nltk.download('punkt')
 
@@ -50,30 +47,169 @@ class CNNBusinessHeadlines(scrapy.Spider):
 
         now = datetime.now()
 
-        self.OUTPUT_DIR = os.path.join(CWD, 'dataset', f'CNN_{self.sections}_headlines_{now.month}_{now.day}_{now.year}_{now.hour}_{now.minute}')
-        self.LOG_DIR = os.path.join(self.OUTPUT_DIR, 'log')
-        self.LOG_FILE = os.path.join(self.LOG_DIR, f'CNN_{self.sections}_headlines_{now.month}_{now.day}_{now.year}_{now.hour}_{now.minute}_log.txt')
+        self.OUTPUT_DIR = os.path.join(CWD, 'dataset', 'headlines', f"CNN_{now.month}_{now.day}_{now.year}")
+        self.OUTPUT_FILE = os.path.join(self.OUTPUT_DIR, f'headlines.csv')
 
         check_and_create_dir(self.OUTPUT_DIR)
-        check_and_create_dir(self.LOG_DIR)
 
-        reload(logging)
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.WARNING)
-        logging.basicConfig(level = logging.ERROR, handlers = [logging.FileHandler(self.LOG_FILE, mode= 'w'), stream_handler])
+        try:
+            print(f"{self.OUTPUT_FILE} loaded")
+            self.df = pd.read_csv(self.OUTPUT_FILE)
+        except Exception as e:
+            print(f"{self.OUTPUT_FILE} doesn't exist, creating one ... ")
+            self.df = pd.DataFrame([], columns = ['title', 'date', 'text', 'authors', 'source', 'url', 'image_url'])
+            print(str(e))
+
+        self.counter_url = Counter()
+
+        # Add all scraped URLs to counter to prevent duplication
+        for index, row in self.df.iterrows():
+            if row['url'] not in self.counter_url:
+                self.counter_url[row['url']]+=1
+            
+        print('counter url =', str(self.counter_url))
     
     def start_requests(self):
-        url = 'https://www.cnn.com/business'
+        print('Fetching business healdines')
+        url = f'https://www.cnn.com/{self.sections}'
         yield SplashRequest(url, callback = self.parse, args = {'wait': 5})
     
     def parse(self, response):
         logging.info(f"{datetime.now()} Fetched {response.url}")
-        print(response)
         html_path = os.path.join(self.OUTPUT_DIR, 'cnn_headlines.html')
+        html = response.body
 
         with open(html_path, 'wb') as file:
             logging.info(f"Saved html to {html_path}")
             file.write(response.body)
+        
+        self._process_html(html)
+    
+    def _process_html(self, html):
+        links = self._get_all_links(html)
+
+        data_list = list()
+
+        for link in links:
+            try:
+                if self._check_if_url_exists(link):
+                    print("URL existed")
+                    continue
+                
+                self.counter_url[link] += 1
+
+                data = self._fetch_article(link)
+                data_list.append([
+                    data['title'],
+                    data['date'],
+                    data['text'],
+                    data['authors'],
+                    data['source'],
+                    data['url'],
+                    data['image_url']
+                ])
+
+            except Exception as e:
+                print(str(e))
+                continue
+        
+        new_df = pd.DataFrame(data_list, columns = ['title', 'date', 'text', 'authors', 'source', 'url', 'image_url'])
+        self.df = pd.concat([self.df, new_df], ignore_index=True)
+        self.df.to_csv(self.OUTPUT_FILE, index = False)
+    
+    def _check_if_url_exists(self, url):
+        if url in self.counter_url:
+            return True
+        return False
+
+
+    def _fetch_article(self, link):
+        '''
+            Fetch article from link
+            @params
+                string link
+            @return
+                list of error links
+        '''
+        link_errors = list()
+
+        try:
+            logging.info(f"Extracting news at {link}")
+            print(f"Extracting news at {link}")
+            article = Article(link)
+            article.download()
+            article.parse()
+
+            # newspaper3k often parse with not all the article content. 
+            # If there's a Read More text at the last line, trigger manual text parsing
+            # This is an unknown error in newspaper3k
+            
+            last_line = article.text.split('\n')[-1]
+            
+            if last_line == 'Read More':
+                text = self._manually_get_text(link)
+                article.text = text
+
+            #Filter out non-alphabet and non-digits character
+            article.title = re.sub(r'[^a-zA-Z\s0-9]+', '', article.title)
+
+            return {
+                'title': article.title,
+                'date': article.publish_date,
+                'text': article.text,
+                'authors': article.authors,
+                'source': 'CNN',
+                'url': link,
+                'image_url': article.top_image
+            }
+
+            # if self.db.get_by_title(article.title) is not None:
+            #     logging.warning(f"Skipping article '{article.title}' because it's already saved in database")
+            #     return
+        except Exception as e:
+            print(str(e))
+            return link_errors
+        
+    def _manually_get_text(self, link):
+
+        html = requests.get(link)
+
+        soup = BeautifulSoup(html.text)
+
+        elements = soup.find_all(recursive=True, )
+
+        text_element = list()
+        for e in elements:
+            try:
+                for element_class in e.attrs['class']:
+                    if element_class.startswith('zn-body__paragraph'):
+                        text_element.append(e)
+            except KeyError as error:
+                continue
+                    
+        text = list()
+        for e in text_element:
+            text.append(e.text)
+
+        print("Start from here:")
+        return '\n'.join(text)
+
+    def _get_all_links(self, html):
+        soup = bs(html)
+
+        all_a_tags = soup.find_all('a')
+        hrefs = list()
+        for a in all_a_tags:
+            try:
+                href = a.attrs['href']
+                if href.endswith(f'index.html'):
+                    hrefs.append(f"https://www.cnn.com{href}")
+
+            except Exception as e:
+                print(str(e))
+                continue
+        
+        return hrefs
         
 
 
